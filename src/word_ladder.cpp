73039b71c951793edc9df586a1f812cb0ddc0878
Manan Jaiswal <z5481212@ad.unsw.edu.au>
2024-05-30 20:05:20 +1000

    improved style fo r reduce set and layed out bfs

--- src/word_ladder.cpp
+++ src/word_ladder.cpp
@@ -1,8 +1,10 @@
 #include "word_ladder.h"
+#include <unordered_set>
 
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <queue>
 
 auto word_ladder::read_lexicon(const std::string &path) -> std::unordered_set<std::string> {
 	std::unordered_set<std::string> set;
@@ -18,6 +20,14 @@ auto word_ladder::read_lexicon(const std::string &path) -> std::unordered_set<st
 	return set;
 }
 
+auto reduce_set(std::unordered_set<std::string> &lexicon, const std::string &from) {
+	size_t size = from.size();
+	std::erase_if(lexicon, [size](auto& x) {
+		return x.size() == size;
+	});
+
+	// any other improvements
+}
 
 auto word_ladder::generate(
 	const std::string &from,
@@ -28,6 +38,24 @@ auto word_ladder::generate(
 	(void)to;
 	(void)lexicon;
 	// remove this line once implemented
+
+	std::unordered_set<std::string> copy_lexicon = lexicon;
+	reduce_set(copy_lexicon, from);
+
+	std::queue<std::string> q;
+	std::unordered_set<std::string> visited;
+
+	q.push(from);
+	visited.insert(from);
+	
+	while (!q.empty()) {
+		std::string curr = q.front();
+		q.pop();
+		std::cout << curr << " ";
+
+		
+
+	}
 
 	return {};
 }
