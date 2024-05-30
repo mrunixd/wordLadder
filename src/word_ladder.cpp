4d57b7e40773035ab1c425371a6723078b643fe1
Manan Jaiswal <z5481212@ad.unsw.edu.au>
2024-05-30 23:13:56 +1000

    implemented get neighbours and bfs i think

--- src/word_ladder.cpp
+++ src/word_ladder.cpp
@@ -1,6 +1,7 @@
 #include "word_ladder.h"
 #include <unordered_set>
 
+#include <cstddef>
 #include <fstream>
 #include <iostream>
 #include <string>
@@ -29,33 +30,56 @@ auto reduce_set(std::unordered_set<std::string> &lexicon, const std::string &fro
 	// any other improvements
 }
 
+std::vector<std::string> generate_neighbours(const std::string &word, const std::unordered_set<std::string> &lexicon) {
+	std::vector<std::string> neighbours;
+	for (size_t i = 0; i < word.size(); i++) {
+		std::string temp = word;
+		for (char c = 'a'; c <= 'z'; ++c) {
+			temp[i] = c;
+			if (lexicon.find(temp) != lexicon.end()) {
+				neighbours.push_back(temp);
+			}
+		}
+	}
+	return neighbours;
+}
+
 auto word_ladder::generate(
 	const std::string &from,
 	const std::string &to,
 	const std::unordered_set<std::string> &lexicon
 ) -> std::vector<std::vector<std::string>> {
-	(void)from;
-	(void)to;
-	(void)lexicon;
-	// remove this line once implemented
-
+	std::vector<std::vector<std::string>> results;
 	std::unordered_set<std::string> copy_lexicon = lexicon;
 	reduce_set(copy_lexicon, from);
 
-	std::queue<std::string> q;
-	std::unordered_set<std::string> visited;
+	std::queue<std::vector<std::string>> q;
+	q.push({from});
 
-	q.push(from);
-	visited.insert(from);
-	
-	while (!q.empty()) {
-		std::string curr = q.front();
-		q.pop();
-		std::cout << curr << " ";
+	bool found = false;
+	while (!q.empty() && !found) {
+		size_t level_size = q.size();
 
-		
+		for (size_t i = 0; i < level_size; i++) {
+			auto path = q.front();
+			q.pop();
+			std::string curr = path.back();
 
+			std::vector<std::string> neighbours = generate_neighbours(curr, lexicon);
+			for (auto &neighbour : neighbours) {
+				if (neighbour == to) {
+					found = true;	
+					std::vector<std::string> new_path = path;
+					new_path.push_back(neighbour);
+					results.push_back(new_path);
+				} else {
+					std::vector<std::string> new_path = path;
+					new_path.push_back(neighbour);
+					q.push(new_path);
+				}
+				copy_lexicon.erase(neighbour);
+			}
+		}
 	}
-
-	return {};
+	return results;
 }
