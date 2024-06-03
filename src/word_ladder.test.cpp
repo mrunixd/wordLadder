cba10f4cfeae077954199e326cde5b1325696fdc
c-ientific <z5481212@ad.unsw.edu.au>
2024-06-03 23:12:27 +1000

    test fix

--- src/word_ladder.test.cpp
+++ src/word_ladder.test.cpp
@@ -2,9 +2,8 @@
 
 #include <catch2/catch.hpp>
 
-TEST_CASE("word_ladder::read_lexicon works as expected") {
-	std::unordered_set<std::string> test_set = {"Hello", "My", "Name", "Is", "Manan"};
-	CHECK(test_set == word_ladder::read_lexicon("/home/manan/COMP6771/ass1/src/test.txt"));
+TEST_CASE("read_lexicon works as expected") {
+	CHECK(127142 == word_ladder::read_lexicon("./english.txt").size());
 }
 
 TEST_CASE("at -> it") {
