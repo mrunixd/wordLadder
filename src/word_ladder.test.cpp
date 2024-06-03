218f9e3dd8c3d7f7f37e6ebc5d1c950b714d3921
c-ientific <z5481212@ad.unsw.edu.au>
2024-06-03 23:21:23 +1000

    test for generate

--- src/word_ladder.test.cpp
+++ src/word_ladder.test.cpp
@@ -1,6 +1,8 @@
 #include "word_ladder.h"
 
 #include <catch2/catch.hpp>
+#include <unordered_set>
+#include <cerrno>
 
 TEST_CASE("read_lexicon works as expected") {
 	CHECK(127142 == word_ladder::read_lexicon("./english.txt").size());
@@ -17,6 +19,22 @@ TEST_CASE("at -> it") {
 	};
 
 	auto const ladders = word_ladder::generate("at", "it", lexicon);
+
+	CHECK(ladders == expected);
+}
+
+TEST_CASE("fold -> mole") {
+	auto const lexicon = std::unordered_set<std::string>{
+		"fold",
+		"mold",
+		"told",
+		"mole"
+	};
+
+	auto const expected = std::vector<std::vector<std::string>>{
+		{"fold", "mold", "mole"}
+	};
+	auto const ladders = word_ladder::generate("fold", "mole", lexicon);
 
 	CHECK(ladders == expected);
 }
