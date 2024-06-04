d7ff69a1c0a5b265eafce32ad70cecf8d5a8e637
c-ientific <z5481212@ad.unsw.edu.au>
2024-06-04 17:48:46 +1000

    more complex test

--- src/word_ladder.test.cpp
+++ src/word_ladder.test.cpp
@@ -2,7 +2,6 @@
 
 #include <catch2/catch.hpp>
 #include <unordered_set>
-#include <cerrno>
 
 TEST_CASE("read_lexicon works as expected") {
 	CHECK(127142 == word_ladder::read_lexicon("./english.txt").size());
@@ -54,12 +53,39 @@ TEST_CASE("work -> play") {
 		"pean",
 		"plan",
 		"worm",
+		"peat",
+		"plat",
+		"porn",
+		"pirn",
+		"pert",
+		"pian",
+		"port",
+		"word",
+		"wood",
+		"pood",
+		"plod",
+		"ploy",
+		"wort",
+		"bort",
+		"boat",
+		"blat",
+		"wert",
+		"worn"
 	};
 
 	auto const expected = std::vector<std::vector<std::string>>{
 		{"work", "fork", "form", "foam", "flam", "flay", "play"},
 		{"work", "pork", "perk", "peak", "pean", "plan", "play"},
-		{"work", "worm", "form", "foam", "flam", "flay", "play"}
+		{"work", "pork", "perk", "peak", "peat", "plat", "play"},
+		{"work", "pork", "perk", "pert", "peat", "plat", "play"},
+		{"work", "pork", "porn", "pirn", "pian", "plan", "play"},
+		{"work", "pork", "port", "pert", "peat", "plat", "play"},
+		{"work", "word", "wood", "pood", "plod", "ploy", "play"},
+		{"work", "worm", "form", "foam", "flam", "flay", "play"},
+		{"work", "worn", "porn", "pirn", "pian", "plan", "play"},
+		{"work", "wort", "bort", "boat", "blat", "plat", "play"},
+		{"work", "wort", "port", "pert", "peat", "plat", "play"},
+		{"work", "wort", "wert", "pert", "peat", "plat", "play"}
 	};
 
 	auto const ladder = word_ladder::generate("work", "play", lexicon);
